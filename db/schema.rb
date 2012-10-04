fc40409dfede9468150d8c9ff992e183f0dc72c4
Mark Joseph Tan <markjosephltan@gmail.com>
2012-10-04 14:02:39 -0700

    Added drop down for Categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121003165128) do
+ActiveRecord::Schema.define(:version => 20121003193953) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20121003165128) do
     t.integer  "bid_duration"
     t.datetime "created_at",                                       :null => false
     t.datetime "updated_at",                                       :null => false
+    t.string   "display_title"
   end
 
   create_table "models", :force => true do |t|
