292a8fdb380fa168880b80cbc6d1e8867c7f4e16
Andres Ramirez <andresrv18@hotmail.com>
2012-11-04 19:29:26 -0800

    - test data

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,6 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
+    it { should have_selector('title', text: full_title('Home')) }
   end
 end
